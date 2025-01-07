308e95ae356d8ef822d2cfb1f71a0f36499d3cba
Timelaying <makanjuola.timi@gmail.com>
2025-01-07 00:01:58 +0000

    button fix

--- golb/src/app/page.js
+++ golb/src/app/page.js
@@ -1,12 +1,12 @@
 import { Button } from "@/components/ui/button";
-import Link from "next/link";
+import Link from "next/link"; // using Link for routing
 
 export default function Landingpage() {
   return (
     <div>
       <h1>Welcome</h1>
       <div>
-        <Link href="/Frontend/page">
+        <Link href="/Frontend"> 
           <Button>Click Me</Button>
         </Link>
       </div>
