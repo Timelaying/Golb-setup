1e5ac5be306ab096062aeb2bc56f5c7358d115a3
Timelaying <makanjuola.timi@gmail.com>
2025-01-08 17:26:00 +0000

    setting username and password for zod

--- golb/src/app/Frontend/page.js
+++ golb/src/app/Frontend/page.js
@@ -1,8 +1,34 @@
-export default function Frontpage (){
-    return <div>
-        <h1>Input your Username and Password</h1>
-        <div>
-            
-        </div>
-    </div> 
+//imports
+"use client";
+
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod"; // for setting Schema just for data to follow the structure
+
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+
+const formSchema = z.object({
+    username: z.string().min(2, {
+      message: "Username must be at least 2 characters.",
+    }),
+    password: z.string().min(8)
+  })
+
+export default function Frontpage() {
+  return (
+    <div>
+      <h1>Input your Username and Password</h1>
+      <div></div>
+    </div>
+  );
 }
