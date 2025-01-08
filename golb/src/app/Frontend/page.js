68c06cdb299efc082aa9d246a4a460c726cc6791
Timelaying <makanjuola.timi@gmail.com>
2025-01-08 17:36:20 +0000

    testing the login

--- golb/src/app/Frontend/page.js
+++ golb/src/app/Frontend/page.js
@@ -1,8 +1,10 @@
+import LoginForm from "./LoginForm"
+
 export default function Frontpage (){
     return <div>
         <h1>Input your Username and Password</h1>
         <div>
-            
+            LoginForm()
         </div>
     </div> 
 }
