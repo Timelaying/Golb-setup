647ab1e4214406e5cc464d1f1ae3bc438002959d
Timelaying <makanjuola.timi@gmail.com>
2025-01-12 12:01:30 +0000

    register to login page

--- golb/src/app/Frontend/page.js
+++ golb/src/app/Frontend/page.js
@@ -1,10 +1,15 @@
 import LoginForm from "./LoginForm"
+import RegisterForm from "./RegisterForm"
 
 export default function Frontpage (){    //<LoginForm renders the jsx
     return (<div>
         <h1>Input your Username and Password</h1>
         <div>
             <LoginForm /> 
+        </div>
+        <h1> New User ?</h1>
+        <div>
+            <RegisterForm />
         </div>
     </div> 
     
