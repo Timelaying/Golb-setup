f9cee3cd96017ac589140536f2479efef49df7c0
Timelaying <makanjuola.timi@gmail.com>
2025-01-13 11:31:43 +0000

    fixing routing

--- golb/src/app/Frontend/page.js
+++ golb/src/app/Frontend/page.js
@@ -5,13 +5,15 @@ export default function Frontpage (){    //<LoginForm renders the jsx
     return (<div>
         <h1>Want to get in on th gist?</h1>
         <div>
-        <Link href="/Forms/LoginForm"> 
+        <Link href="Frontend/Forms/LoginForm"> 
           <Button>Login</Button>
         </Link>
         </div>
         <h1> New User ? Register</h1>
         <div>
-            <RegisterForm />
+        <Link href="Frontend/Forms/RegisterForm"> 
+          <Button>Login</Button>
+        </Link>
         </div>
     </div> 
     
