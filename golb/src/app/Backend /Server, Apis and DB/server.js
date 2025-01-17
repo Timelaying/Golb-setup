adade7e21d266009dfeb4410be195affb87776a0
Timelaying <makanjuola.timi@gmail.com>
2025-01-17 14:01:13 +0000

    configuring server

--- golb/src/app/Backend /Server, Apis and DB/server.js
+++ golb/src/app/Backend /Server, Apis and DB/server.js
@@ -8,6 +8,7 @@ const cors = require('cors');
 const bcrypt = require('bcrypt'); // Import bcrypt
 const authRoutes = require("./AuthRoute"); // Import the router module
 const registerRoute = require("./RegisterRoute")
+const forgetandreset = require("./ForgetAndReset")
 
 const app = express();
 const PORT = 5000;
@@ -20,6 +21,7 @@ app.use(bodyParser.json());
 // Mount the auth routes on the `/api` path
 app.use("/api", registerRoute);
 app.use("/api", authRoutes);
+app.use("/api", forgetandreset);
 
 // Start the server
 app.listen(PORT, () => {
