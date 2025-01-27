e024f04568bdda1e173087553c9adf1ba814e575
Timelaying <makanjuola.timi@gmail.com>
2025-01-27 18:57:46 +0000

    get in on the gist

--- golb/src/app/Frontend/page.js
+++ golb/src/app/Frontend/page.js
@@ -3,7 +3,7 @@ import { Button } from "@/components/ui/button"
 
 export default function Frontpage (){    //<LoginForm renders the jsx
     return (<div>
-        <h1>Want to get in on th gist?</h1>
+        <h1>Want to get in on the gist?</h1>
         <div>
         <Link href="Frontend/Forms/LoginForm"> 
           <Button>Login</Button>
