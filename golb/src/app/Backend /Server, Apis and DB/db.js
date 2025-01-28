30fd56cbea3bc359a3aadf4b45848c7a642f9672
Timelaying <makanjuola.timi@gmail.com>
2025-01-28 18:37:36 +0000

    adding full name to database

--- golb/src/app/Backend /Server, Apis and DB/db.js
+++ golb/src/app/Backend /Server, Apis and DB/db.js
@@ -16,6 +16,7 @@ const createTableQuery = `
     name VARCHAR(100) NOT NULL,
     email VARCHAR(150) UNIQUE NOT NULL,
     password VARCHAR(255) NOT NULL,
+    full_name VARCHAR(255) NOT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
   );
 
