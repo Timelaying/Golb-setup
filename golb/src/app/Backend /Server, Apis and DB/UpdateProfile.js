b94f56239dfad71584bf0a4d92783204de2175a4
Timelaying <makanjuola.timi@gmail.com>
2025-01-31 19:22:31 +0000

    various import for update profile

--- golb/src/app/Backend /Server, Apis and DB/UpdateProfile.js
+++ golb/src/app/Backend /Server, Apis and DB/UpdateProfile.js
@@ -1,3 +1,12 @@
+require("dotenv").config();
+
+const express = require("express");
+const router = express.Router();
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const nodemailer = require("nodemailer");
+const pool = require("./db");
+
 // Route to update profile information
 router.put("/update-profile", async (req, res) => {
     const { userId, location, bio, profile_picture } = req.body;
