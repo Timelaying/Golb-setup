61960d5eb201b90344552d12e788fbd7938f99cd
Timelaying <makanjuola.timi@gmail.com>
2025-02-24 11:16:41 +0000

    debugging

--- golb/src/app/Backend /Server, Apis and DB/UpdateProfile.js
+++ golb/src/app/Backend /Server, Apis and DB/UpdateProfile.js
@@ -10,7 +10,7 @@ const storage = multer.diskStorage({
     cb(null, 'uploads/');
   },
   filename: function (req, file, cb) {
-    cb(null, Date.now() + path.extname(file.originalname)); // Append the file extension
+    cb(null, Date.now() + path.extname(file.originalname));
   }
 });
 
