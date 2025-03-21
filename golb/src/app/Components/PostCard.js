0ba9dabb18bfb6e73b81c097d85fe4f43e04f696
Timelaying <makanjuola.timi@gmail.com>
2025-03-21 16:40:47 +0000

    postcard update

--- golb/src/app/Components/PostCard.js
+++ golb/src/app/Components/PostCard.js
@@ -1,14 +1,14 @@
 import LikeButton from "./LikeButton";
 import CommentBox from "./CommentBox";
 
-const PostCard = ({ post, userId }) => {
+const PostCard = ({ post }) => {
   return (
-    <div className="bg-gray-800 p-4 rounded shadow mb-6 text-white border border-gray-700">
-      <h2 className="text-xl font-semibold">{post.title}</h2>
-      <p className="text-gray-300 mb-2">{post.content}</p>
+    <div className="bg-gray-800 text-gray-100 border border-gray-700 p-4 rounded-lg shadow-md">
+      <h2 className="text-xl font-semibold text-gray-200">{post.title}</h2>
+      <p className="text-gray-300">{post.content}</p>
 
-      <LikeButton postId={post.id} userId={userId} />
-      <CommentBox postId={post.id} userId={userId} />
+      <LikeButton postId={post.id} />
+      <CommentBox postId={post.id} />
     </div>
   );
 };
