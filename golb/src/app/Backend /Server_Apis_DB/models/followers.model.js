108330ceb39450417e07b2c1f6a9bff7a35c42d0
Timelaying <makanjuola.timi@gmail.com>
2025-03-24 20:59:02 +0000

    followers model

--- golb/src/app/Backend /Server_Apis_DB/models/followers.model.js
+++ golb/src/app/Backend /Server_Apis_DB/models/followers.model.js
@@ -9,21 +9,33 @@ async function isFollowing(followerId, followingId) {
 }
 
 async function followUser(followerId, followingId) {
-  await pool.query(
+  return await pool.query(
     "INSERT INTO followers (follower_id, following_id) VALUES ($1, $2)",
     [followerId, followingId]
   );
 }
 
 async function unfollowUser(followerId, followingId) {
-  await pool.query(
+  return await pool.query(
     "DELETE FROM followers WHERE follower_id = $1 AND following_id = $2",
     [followerId, followingId]
   );
+}
+
+async function getFollowersForUser(userId) {
+  const res = await pool.query(
+    `SELECT users.id, users.username, users.profile_picture
+     FROM followers 
+     JOIN users ON followers.follower_id = users.id 
+     WHERE followers.following_id = $1`,
+    [userId]
+  );
+  return res.rows;
 }
 
 module.exports = {
   isFollowing,
   followUser,
   unfollowUser,
+  getFollowersForUser,
 };
