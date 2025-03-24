ab5c0a2855e6ffc39c5ddc5d2d8a95455f99cb4d
Timelaying <makanjuola.timi@gmail.com>
2025-03-24 21:43:50 +0000

    user model update

--- golb/src/app/Backend /Server_Apis_DB/models/users.model.js
+++ golb/src/app/Backend /Server_Apis_DB/models/users.model.js
@@ -15,11 +15,12 @@ async function findUserByUsername(username) {
 // Create a new user
 async function createUser({ name, username, email, password }) {
   const result = await pool.query(
-    "INSERT INTO users (name, username, email, password) VALUES ($1, $2, $3, $4) RETURNING *",
+    "INSERT INTO users (name, username, email, password) VALUES ($1, $2, $3, $4) RETURNING id, name, username, email, created_at",
     [name, username, email, password]
   );
   return result.rows[0];
 }
+
 
 async function updateUserProfile({ userId, location, bio, profilePicture }) {
   const result = await pool.query(
