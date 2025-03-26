ece1965da0ba2b84e5367bbf05d29a2769048201
Timelaying <makanjuola.timi@gmail.com>
2025-03-26 16:18:29 +0000

    page header.jsx

---
+++ golb/src/app/Components/PageHeader.jsx
@@ -0,0 +1,9 @@
+// components/ui/PageHeader.jsx
+export default function PageHeader({ title }) {
+    return (
+      <h1 className="text-3xl font-semibold text-center mb-6">
+        {title}
+      </h1>
+    );
+  }
+  
