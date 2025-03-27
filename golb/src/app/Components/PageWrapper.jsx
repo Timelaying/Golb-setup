3f8f3452cee668e5168f671ea466d1cdad26df63
Timelaying <makanjuola.timi@gmail.com>
2025-03-27 22:06:42 +0000

    adding motions

--- golb/src/app/Components/PageWrapper.jsx
+++ golb/src/app/Components/PageWrapper.jsx
@@ -1,9 +1,16 @@
-// components/ui/PageWrapper.jsx
+// components/PageWrapper.jsx
+"use client";
+import { motion } from "framer-motion";
+
 export default function PageWrapper({ children }) {
-    return (
-      <div className="flex flex-col items-center justify-center min-h-screen bg-gray-900 text-gray-100 px-4">
-        {children}
-      </div>
-    );
-  }
-  
+  return (
+    <motion.div
+      initial={{ opacity: 0, y: 16 }}
+      animate={{ opacity: 1, y: 0 }}
+      transition={{ duration: 0.4 }}
+      className="flex flex-col items-center justify-center min-h-screen bg-gray-900 text-gray-100 px-4 md:px-6 lg:px-8"
+    >
+      {children}
+    </motion.div>
+  );
+}
