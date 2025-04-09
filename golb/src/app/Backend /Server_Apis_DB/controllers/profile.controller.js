b2e3d4736a66f167da1c83755beb6a240342422a
Timelaying <makanjuola.timi@gmail.com>
2025-04-09 13:49:16 +0100

    profile controller updat e

--- golb/src/app/Backend /Server_Apis_DB/controllers/profile.controller.js
+++ golb/src/app/Backend /Server_Apis_DB/controllers/profile.controller.js
@@ -2,6 +2,9 @@
 
 const { getUserProfile } = require("../models/users.model");
 const { getFollowerStats } = require("../models/followers.model");
+const path = require("path");
+const { getUploadPathForUser } = require("../utils/uploads");
+const { updateUserProfile } = require("../models/users.model");
 const ERROR_CODES = require("../utils/errorCodes");
 
 const handleGetProfile = async (req, res) => {
@@ -25,6 +28,28 @@ const handleGetProfile = async (req, res) => {
     followersCount: parseInt(stats.followers_count, 10),
     followingCount: parseInt(stats.following_count, 10),
   });
+};
+
+exports.handleProfileUpdate = async (req, res) => {
+  const { userId, location, bio } = req.body;
+
+  const profilePicturePath = req.file
+    ? `/uploads/users/${req.usernameForFile}/${req.file.filename}`
+    : null;
+
+  const updatedUser = await updateUserProfile({
+    userId,
+    location,
+    bio,
+    profilePicture: profilePicturePath,
+  });
+
+  if (!updatedUser) {
+    return res.status(404).json(errorCodes.USER_NOT_FOUND);
+  }
+
+  updatedUser.profile_picture = `http://localhost:5000${updatedUser.profile_picture}`;
+  res.status(200).json(updatedUser);
 };
 
 module.exports = {
